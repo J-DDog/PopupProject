6a0134dffdf2cced14cd5c33cdba30c2c74ab342
Traitortots <jker3169@740s-pl-116.local>
2015-09-24 09:35:42 -0600

    Added a Thingy instance and then added some modules to catch errors

--- src/input/controller/InputController.java
+++ src/input/controller/InputController.java
@@ -1,11 +1,13 @@
 package input.controller;
 
+import input.model.Thingy;
 import input.view.PopupDisplay;
 
 public class InputController
 {
 	
 	private PopupDisplay myPopups;
+	private Thingy myTestThingy;
 	
 	public InputController()
 	{
@@ -13,11 +15,82 @@ public class InputController
 		
 	}
 	
+	/*
+	 * The Program Starting Method.
+	 */
 	public void start()
 	{
 		
-		String temp = myPopups.grabAnswer("Look Words!");
-		myPopups.showResponse("You typed in: " + temp);
+		String myName = myPopups.grabAnswer("Type in your name: ");
+		myPopups.showResponse("You typed in: " + myName);
+		
+		String ageTemp = myPopups.grabAnswer("Type in your age: ");
+		int myAge;
+		if(isInteger(ageTemp))
+		{
+			myAge = Integer.parseInt(ageTemp);
+		}
+		else
+		{
+			myAge = -9999999;
+		}
+		
+		myPopups.showResponse("You typed in: " + myAge);
+		
+		String weightTemp = myPopups.grabAnswer("Type in your weight: ");
+		double myWeight;
+		if(isDouble(ageTemp))
+		{
+			myWeight = Double.parseDouble(weightTemp);
+		}
+		else
+		{
+			myWeight = -9999999;
+		}
+		
+		myPopups.showResponse("You typed in: " + myWeight);
+		
+		myTestThingy = new Thingy();
+	}
+	
+	/*
+	 * Check if string can be converted to integer.
+	 */
+	private boolean isInteger(String input)
+	{
+		boolean isInt = false;
+		
+		try
+		{
+			int myAge = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch(NumberFormatException error)
+		{
+			myPopups.showResponse("not an int - bad value will be used");
+		}
+		
+		return isInt;
+	}
+	
+	/*
+	 * Check if string can be converted to double.
+	 */
+	private boolean isDouble(String input)
+	{
+		boolean isDouble = false;
+		
+		try
+		{
+			double myWeight = Double.parseDouble(input);
+			isDouble = true;
+		}
+		catch(NumberFormatException error)
+		{
+			myPopups.showResponse("not a double = bad value will be used");
+		}
+		
+		return isDouble;
 	}
 	
 }

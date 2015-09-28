25653524bdedb28c6cd9f9af2a16f859fa354d0f
Traitortots <jker3169@740s-pl-116.local>
2015-09-28 09:27:19 -0600

    Added Documentation for our PopupDisplay class

--- src/input/view/PopupDisplay.java
+++ src/input/view/PopupDisplay.java
@@ -9,12 +9,21 @@ import javax.swing.JOptionPane;
 public class PopupDisplay
 {
 	
+	/**
+	 * Displays the supplied text as a popup window.
+	 * @param worsFromSomewhere The text to be displayed
+	 */
 	public void showResponse(String wordsFromSomewhere)
 	{
 		JOptionPane.showMessageDialog(null, wordsFromSomewhere);
 		
 	}
 	
+	/**
+	 * Displays a popup with a field to type in a responce.
+	 * @param stuff The text of the popup window.
+	 * @return The users input as a string ata type.
+	 */
 	public String grabAnswer(String stuff)
 	{
 		String answer = "";
